73eba3e083b112f926a33a4b49a72c68694f3c0f
DevvSakib <devvsakib@gmail.com>
2023-02-24 20:20:28 +0600

    search function added

--- movie-watchlist/src/components/Search/Search.jsx
+++ movie-watchlist/src/components/Search/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-const Search = ({ searchValue, setSearchValue }) => {
+const Search = ({ searchValue, setSearchValue, searchDrama }) => {
     const [count, setCount] = useState(0);
     return (
         <div className="form-control mb-10">
@@ -15,7 +15,7 @@ const Search = ({ searchValue, setSearchValue }) => {
                     <input type="text" placeholder="Search Movie…" className="input input-bordered"
                         value={searchValue}
                         onChange={e => setSearchValue(e.target.value)} />
-                    <button className="btn btn-square">
+                    <button className="btn btn-square" onClick={searchDrama}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </button>
                 </div>
--- movie-watchlist/src/pages/Home.jsx
+++ movie-watchlist/src/pages/Home.jsx
@@ -4,16 +4,19 @@ import Search from '../components/Search/Search'
 import axios from 'axios'
 
 const Home = () => {
-    const apiUrl = "https://mydramalist-scrape-api.vercel.app/search/q/true-beauty"
     const [dramas, setDramas] = useState([])
     const [searchValue, setSearchValue] = useState('')
+    const apiUrl = `https://mydramalist-scrape-api.vercel.app/search/q/${searchValue}`
 
-    useEffect(() => {
-        axios.get(apiUrl)
+    const searchDrama = () => {
+        console.log(searchValue);
+        fetch(apiUrl)
+            .then(res => res.json())
             .then(data => {
-                setDramas(data.data.results.dramas)
+                setDramas(data.results.dramas)
             })
-    }, [])
+            setDramas([])
+    }
 
 
     return (
@@ -21,15 +24,18 @@ const Home = () => {
             <Search
                 value={searchValue}
                 setSearchValue={setSearchValue}
+                searchDrama={searchDrama}
             />
-            <div className='grid gap-8 grid-cols-1 md:grid-cols-3'>
+            <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     dramas.map((drama, idx) => (
-                        <MovieCard
-                            drama={drama}
-                            key={idx}
-                        />
-                ))
+                        drama.title !== undefined ?
+                            <MovieCard
+                                drama={drama}
+                                key={idx}
+                                setDramas={setDramas}
+                            /> : "No Drama"
+                    ))
                 }
             </div>
         </div>
