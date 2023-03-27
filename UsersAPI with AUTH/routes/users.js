e358cbcf8be28ec35907e89ac703fb623060771e
DevvSakib <devvsakib@gmail.com>
2023-03-28 01:58:09 +0600

    get user by username

--- UsersAPI with AUTH/routes/users.js
+++ UsersAPI with AUTH/routes/users.js
@@ -3,10 +3,26 @@ import User from "../models/User.js"
 
 const router = express.Router();
 
+// Response JSON, will use to reduce code and DRY
+const resJson = (message, code) => {
+    const jsn = {
+        message: message,
+        code: code
+    }
+    return jsn
+}
+
 // Get all users from database
 router.get("/users", async (req, res) => {
     const users = await User.find({})
     res.json(users)
 })
 
-export default  router;
+// Create new user
+router.post("/user/:username", async (req, res) => {
+    const user = req.params
+    const findUser = await User.findOne({ username: user.username })
+    findUser ? res.json(findUser) : res.json(resJson("not found", 201))
+})
+
+export default router;
