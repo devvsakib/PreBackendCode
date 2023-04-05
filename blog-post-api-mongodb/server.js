b2f1e3b22030262268bdc8dd5ad30333c15eac95
DevvSakib <devvsakib@gmail.com>
2023-04-06 01:47:00 +0600

    server init

---
+++ blog-post-api-mongodb/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors())
+
+
+app.get("/", (req, res) => res.json({ Message: "Hi, I'm working fine✅"}))
+
+app.listen(PORT, ()=>console.log(`Server running at ${PORT}`))
