7c221bdaee77f0d480f007eec9e7cda18b9d4ad8
DevvSakib <devvsakib@gmail.com>
2023-04-06 02:00:51 +0600

    server init

--- blog-post-api-mongodb/server.js
+++ blog-post-api-mongodb/server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors())
 
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => res.json({ Message: "Hi, I'm working fine✅"}))
 
