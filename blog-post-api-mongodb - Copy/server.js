62eafc78739029119e54661157022f40bc470aab
DevvSakib <devvsakib@gmail.com>
2023-04-27 14:04:50 +0600

    Revert "Revert "user route added""
    This reverts commit 88192146bb2e73a454165a560a2465aa7c1dd3a8.

--- blog-post-api-mongodb - Copy/server.js
+++ blog-post-api-mongodb - Copy/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { productRouter } from "./routes/products.js";
 import { categoryRouter } from "./routes/category.js";
+import { userRouter } from "./routes/users.js"
 dotenv.config();
 
 
@@ -12,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
-
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -28,6 +28,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.use("/", productRouter)
 app.use("/categories", categoryRouter)
+app.use("/user", userRouter)
 
 app.get("/*", (req, res) => res.json({ Message: "Hi, I'm working fine✅" }))
 
